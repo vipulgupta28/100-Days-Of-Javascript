90dcae29e5f661552e4b5253234164b365b53651
Vipul Gupta <121814604+vipulgupta28@users.noreply.github.com>
2024-12-15 16:30:48 +0530

    script.js

---
+++ Typing Test/script.js
@@ -0,0 +1,13 @@
+//  Rest entire main javascript is internal in test_1_min.js test_3_min.js and test_5_min.js
+
+function start1() {
+    window.location.href = "test_1_min.html";
+}
+
+function start3(){
+    window.location.href = "test_3_min.html";
+}
+
+function start5(){
+    window.location.href = "test_5_min.html";
+}
